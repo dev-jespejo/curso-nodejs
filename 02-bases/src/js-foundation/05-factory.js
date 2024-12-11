2e849d11fa6553a4ce5801599315798daae53f09
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2024-12-11 16:01:35 -0300

    Factory Functions - Necesidad

--- 02-bases/src/js-foundation/05-factory.js
+++ 02-bases/src/js-foundation/05-factory.js
@@ -1,11 +1,14 @@
+const { v4:uuidv4 } = require('uuid');
+const getAge = require('get-age');
+
 
 const buildPerson = ({name, birthdate}) => {
     
     return {
-        id: new Date().getTime(),
+        id: uuidv4(),
         name,
         birthdate,
-        age: new Date().getFullYear() - new Date(birthdate).getFullYear()
+        age: getAge(birthdate)
     }
     
 }
