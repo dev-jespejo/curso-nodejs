448a0c69e8d54130696d280423a2d8a8548cbd60
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2024-12-11 15:37:12 -0300

    Arrow Functions

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -3,14 +3,13 @@
 // const {emailTemplate} = require('./js-foundation/01-template');
 // require('./js-foundation/02-destructuring');
 
-const {getUserByid} = require('./js-foundation/03-callbacks');
+const {getUserByid} = require('./js-foundation/04-arrow');
 
 const id = 1;
 
-getUserByid(id, function(error, user) {
-    if(error) {
-        throw new Error(error);
-    }
+getUserByid(id, (error, user) => {
+    
+    if(error) throw new Error(error);
 
     console.log({user});
     
