0860cbb6b35c692c010ccd423bfa29c8034ea35d
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2024-12-12 11:15:32 -0300

    Async - await

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -4,7 +4,7 @@ const getPokemonById = require('./js-foundation/06-promises');
 
 getPokemonById(1)
     .then((pokemon) => console.log({pokemon}))
-    .catch((err) => console.log('Please try later'))
+    .catch((err) => console.log(err))
     .finally(() => console.log('Finally'));
 
 
--- 02-bases/src/js-foundation/06-promises.js
+++ 02-bases/src/js-foundation/06-promises.js
@@ -1,12 +1,15 @@
 
-const getPokemonById = (id) => {
+const getPokemonById = async(id) => {
 
     const url =  `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-    return fetch(url)
-        .then((resp) =>resp.json())
-        // .then( () => {throw new Error('Pokemon not found')})
-        .then((pokemon) => pokemon.name);
+    const resp = await fetch(url);
+    const pokemon = await resp.json(); 
+
+    // throw new Error('Pokemon not found');
+
+    return pokemon.name;
+
 }
 
 module.exports = getPokemonById;
