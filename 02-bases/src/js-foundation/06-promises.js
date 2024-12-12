b0029e19f580fa2f63484a0fe66314612ef13afe
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2024-12-12 11:48:15 -0300

    Patron adaptador - FetchAPI

--- 02-bases/src/js-foundation/06-promises.js
+++ 02-bases/src/js-foundation/06-promises.js
@@ -1,12 +1,11 @@
 
+const {http} = require('../plugins');
+
 const getPokemonById = async(id) => {
 
     const url =  `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-    const resp = await fetch(url);
-    const pokemon = await resp.json(); 
-
-    // throw new Error('Pokemon not found');
+    const pokemon = await http.get(url);
 
     return pokemon.name;
 
