0ec7329881d661a4a31d3c30acb576dfcbb63bed
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2024-12-16 16:15:23 -0300

    Pruebas en 02-destructuring

--- 02-bases/src/js-foundation/02-destructuring.ts
+++ 02-bases/src/js-foundation/02-destructuring.ts
@@ -8,7 +8,7 @@ const {USERNAME, OS, PUBLIC, npm_lifecycle_script} = process.env;
 
 // console.table({USERNAME, OS, PUBLIC, npm_lifecycle_script});
 
-const characters = ['Flash', 'Superman', 'Green Lantern', 'Batman'];
+export const characters = ['Flash', 'Superman', 'Green Lantern', 'Batman'];
 
 const [,,,batman] = characters;
 
