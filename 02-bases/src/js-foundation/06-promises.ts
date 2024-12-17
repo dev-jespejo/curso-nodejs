bb1bd5aa20c0220d78b98a075e4b83b248306bc3
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2024-12-17 10:51:59 -0300

    Pruebas en 06-promises

--- 02-bases/src/js-foundation/06-promises.ts
+++ 02-bases/src/js-foundation/06-promises.ts
@@ -1,12 +1,18 @@
 
-const {http} = require('../plugins');
+import {httpClientPlugin as http} from '../plugins/http-client.plugin';
+
+// const {http} = require('../plugins');
 
 export const getPokemonById = async(id: string|number):Promise<string> => {
 
-    const url =  `https://pokeapi.co/api/v2/pokemon/${id}`;
-
-    const pokemon = await http.get(url);
-
-    return pokemon.name;
+    try {
+        
+        const url =  `https://pokeapi.co/api/v2/pokemon/${id}`;
+        const pokemon = await http.get(url);
+        return pokemon.name;
+        
+    } catch (error) {
+        throw `Pokemon not found with id ${id}`;
+    }
 
 }
