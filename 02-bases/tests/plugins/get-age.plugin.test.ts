84526f92a702400f2167c9cb8f4b95f31ffebe38
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2024-12-17 15:57:32 -0300

    spyOn - metodos de objetos

--- 02-bases/tests/plugins/get-age.plugin.test.ts
+++ 02-bases/tests/plugins/get-age.plugin.test.ts
@@ -20,7 +20,40 @@ describe('plugins/get-age.plugin.ts', () => {
 
         expect(age).toBe(calculatedAge);
 
-    });   
+    }); 
+    
+    test('getAge should return 0 years', () => {
+        
+        const spy = jest.spyOn(Date.prototype, 'getFullYear').mockReturnValue(1996);
+        
+        const birthdate = '1986-02-14';
+        const age = getAge(birthdate);
+
+        expect(age).toBe(0);   
+        
+        expect(spy).toHaveBeenCalled();
+        
+        jest.restoreAllMocks();
+    });
+
+    test('should return the correct age when the birthday has passed this year', () => {
+        // Mock de Date.now() para retornar una fecha específica en milisegundos
+        const fixedDate = new Date('2024-06-15').getTime();
+        jest.spyOn(global.Date, 'now').mockImplementation(() => fixedDate);
+    
+        const birthdate = '2000-01-15';
+        const age = getAge(birthdate);
+    
+   
+        expect(age).toBe(24);
+    
+        // Limpia el mock
+        jest.restoreAllMocks();
+    });
+    
+    
+    
+
 
 });
 
