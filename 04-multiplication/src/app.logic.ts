c611a9924eff1947786b0a16bbfec7e1faad50e0
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2024-12-19 12:25:22 -0300

    Usando yargs

--- 04-multiplication/src/app.logic.ts
+++ 04-multiplication/src/app.logic.ts
@@ -1,14 +1,17 @@
 import fs from 'fs';
+import { yarg } from './plugins/yargs.plugin';
+
+
+let {b:base, l:limit, s:showTable} = yarg;
 
 let outputMessage = '';
-const base = 5;
 const headerMessage = `
 =======================================
             Tabla del ${base}
 =======================================\n            
 `;
 
-for(let i = 1; i <= 10; i++) {
+for(let i = 1; i <= limit; i++) {
     outputMessage += `${base} x ${i} = ${base*i}\n`;
 }
 
@@ -19,4 +22,9 @@ const outputPath = `outputs`;
 
 fs.mkdirSync(outputPath, {recursive: true});
 fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, outputMessage);
+
+if(showTable){
+    console.log(outputMessage);
+}
+
 console.log('File Created');
