5527fde88e08e883a04958044d266d2faa589c7a
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2024-12-23 10:18:40 -0300

    Refactorizar - Organizar logicamente el codigo

--- 04-multiplication/src/app.ts
+++ 04-multiplication/src/app.ts
@@ -1,4 +1,5 @@
 import { yarg} from "./plugins/yargs.plugin";
+import { ServerApp } from "./presentation/server-app";
 
 
 
@@ -7,8 +8,9 @@ import { yarg} from "./plugins/yargs.plugin";
 })();
 
 async function main(){
+
+    const {b:base, l:limit, s:showTable} = yarg;
     
-    console.log(yarg);
-    
+    ServerApp.run({base, limit, showTable});    
     
 }
