b58c3911c6c9220bd18931cf51c6b387c88e3a1d
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2024-12-27 16:46:04 -0300

    CRON Tasks

--- 05-NOC/src/presentation/server.ts
+++ 05-NOC/src/presentation/server.ts
@@ -1,8 +1,21 @@
 
+import { CronJob } from 'cron';
+
 export class Server {
 
     public static start(){
         console.log('Server started...');
+
+        const job = new CronJob(
+            '*/2 * * * * *', // cronTime
+            function () {
+                const date = new Date();
+                console.log('3 second', date);
+            }, // onTick
+            null, // onComplete
+            true, // start
+        );
+        //job.start()
     }
 
 }
