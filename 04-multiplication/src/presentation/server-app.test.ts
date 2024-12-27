123bf67ba42e973d6462a6c8dea956e83778d2e3
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2024-12-27 10:22:23 -0300

    ServerApp - Ejecutando proceso esperado

--- 04-multiplication/src/presentation/server-app.test.ts
+++ 04-multiplication/src/presentation/server-app.test.ts
@@ -1,6 +1,17 @@
+import { CreateTable } from "../domain/use-cases/create-table.use-case";
+import { SaveFile } from "../domain/use-cases/save-file.use-case";
 import { ServerApp } from "./server-app";
 
 describe('Server App', () => {
+
+    const options = {
+        base: 2,
+        limit: 10,
+        showTable: false,
+        fileDestination: 'test-destination',
+        fileName: 'test-filename',
+    };
+
 
     test('should create ServerApp instance', () => {
 
@@ -11,5 +22,34 @@ describe('Server App', () => {
         expect(typeof ServerApp.run).toBe('function');
 
     });
+
+    test('should run ServerApp with options', () => {
+
+        const logSpy = jest.spyOn(console, 'log');
+        const createTableSpy = jest.spyOn(CreateTable.prototype, 'execute');
+        const saveFileSpy = jest.spyOn(SaveFile.prototype, 'execute');
+      
+
+        ServerApp.run(options);
+
+        expect(logSpy).toHaveBeenCalledTimes(2);
+        expect(logSpy).toHaveBeenCalledWith('Server Running...');
+        expect(logSpy).toHaveBeenLastCalledWith('File created!');
+        
+        expect(createTableSpy).toHaveBeenCalledTimes(1);
+        expect(createTableSpy).toHaveBeenCalledWith({
+            base: options.base, limit: options.limit
+        });
+        
+        expect(saveFileSpy).toHaveBeenCalledTimes(1);
+        expect(saveFileSpy).toHaveBeenCalledWith({
+            fileContent: expect.any(String),
+            fileName: options.fileName,
+            fileDestination: options.fileDestination,
+        });
+
+
+    });
+    
 
 });
